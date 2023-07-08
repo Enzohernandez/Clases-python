61c471234494d8c3d4c1c5313214fbcf0e7deeef
Enzohernandez <pighernandezgutierrez@gmail.com>
2023-07-08 12:50:19 -0500

    Ultima version de api Geko

---
+++ Readme.py
@@ -0,0 +1,31 @@
+"""
+OBJETIVO
+El objetivo de este aplucativo es aplicar conocimientos sobre programacion y aplicarlos a situaciones reales.
+En esta api se evidencia la informacion de la aplicacion "Geko" una aplicacion que contiene la informacion de
+varias criptomonedas tendencias comparacion y utilidad en el mercado de valores bursatil.
+
+UTILIZACION
+Para poder ejecutar la aplicacion primero accede en la terminal al archivo y ejecuta el siguiente
+comando "python manage.py runserver" si logra acceder correctamente al frontend en su motor de busqueda significa
+que lo ah hecho correctamente.
+En ese punto utilizaremos postman para probar los https request mas adelante vendran mas desarollos para
+el frontend de esta api.
+utilize el siguiente link en postamn para probar los requiest que quiere porbar "http://127.0.0.1:8000/api/coinGecko"
+seguido de los request que dese utilizar para obtener el path.
+Si desea obtener informacion mas concreta utilize el signo de interrogacion "?" seguido del parametro query que
+quiere obtener.
+
+AGRADECIMIENTOS
+Esta api esta hasta ahora en desarollo, se agradece a todos los contribuyentes y a el aplicativo de Geko
+por poder usar sus servicios.
+
+"""
+
+
+
+
+
+
+
+
+
--- mysite/api/views.py
+++ mysite/api/views.py
@@ -9,75 +9,75 @@ def index(request):
     return HttpResponse("Hello, world. You're at the api index.")
 
 def GekoAPI_getStatus(request):
-    response= requests.get('https://api.coingecko.com/api/v3/ping')
+    response= requests.get(url + "/v3/ping")
 
-    return HttpResponse(response.content)
-
-def GekoAPI_coinList(request):
-    response= requests.get('https://api.coingecko.com/api/v3/coins/list')
-
-    return HttpResponse(response.content)
-
-def GekoAPI_assetPlatforms(request):
-    response= requests.get('https://api.coingecko.com/api/v3/asset_platforms')
-
-    return HttpResponse(response.content)
-
-def GekoAPI_coinsCategories(request):
-    response= requests.get('https://api.coingecko.com/api/v3/coins/categories')
-
-    return HttpResponse(response.content)
-
-def GekoAPI_exchangesList(request):
-    response= requests.get('https://api.coingecko.com/api/v3/exchanges/list')
-
-    return HttpResponse(response.content)
-
-def GekoAPI_indexes(request):
-    response= requests.get('https://api.coingecko.com/api/v3/indexes')
-
-    return HttpResponse(response.content)
-
-def GekoAPI_derivatives(request):
-    response= requests.get('https://api.coingecko.com/api/v3/derivatives')
-
-    return HttpResponse(response.content)
-
-def GekoAPI_nftsList(request):
-    response= requests.get('https://api.coingecko.com/api/v3/nfts/list')
-
-    return HttpResponse(response.content)
-
-def GekoAPI_exchangeRates(request):
-    response= requests.get('https://api.coingecko.com/api/v3/exchange_rates')
-
-    return HttpResponse(response.content)
-
-def GekoAPI_search(request):
-    print(request.GET)
-    response= requests.get('https://api.coingecko.com/api/v3/search',params=request.GET)
-
-    return HttpResponse(response.content)
-
-def GekoAPI_searchTrending(request):
-    response= requests.get('https://api.coingecko.com/api/v3/search/trending')
-
-    return HttpResponse(response.content)
-
-def GekoAPI_global(request):
-    response= requests.get('https://api.coingecko.com/api/v3/global')
-
-    return HttpResponse(response.content)
+    return HttpResponse(response.content, status= response.status_code)
 
 def GekoAPI_simple_priece(request):
-    response= requests.get(url+'/v3/simple/price',params=request.GET)
+    response= requests.get(url+'/v3/simple/price', params=request.GET)
 
-    return HttpResponse(response.content,status= response.status_code)
+    return HttpResponse(response.content, status= response.status_code)
+
+def GekoAPI_coinList(request):
+    response= requests.get(url + "/v3/coins/list", params=request.GET)
+
+    return HttpResponse(response.content, status= response.status_code)
+
+def GekoAPI_assetPlatforms(request):
+    response= requests.get(url + "/v3/asset_platforms", params=request.GET)
+
+    return HttpResponse(response.content, status= response.status_code)
+
+def GekoAPI_coinsCategories(request):
+    response= requests.get(url + "/v3/coins/categories")
+
+    return HttpResponse(response.content, status= response.status_code)
+
+def GekoAPI_exchangesList(request):
+    response= requests.get(url + "/v3/exchanges", params=request.GET) #revisar pq no cambia
+
+    return HttpResponse(response.content, status= response.status_code)
+
+def GekoAPI_indexes(request):
+    response= requests.get(url + "/v3/indexes", params=request.GET)
+
+    return HttpResponse(response.content, status= response.status_code)
+
+def GekoAPI_derivatives(request):
+    response= requests.get(url + "/v3/derivatives", params=request.GET)
+
+    return HttpResponse(response.content, status= response.status_code)
+
+def GekoAPI_nftsList(request):
+    response= requests.get(url + "/v3/nfts/list", params=request.GET)
+
+    return HttpResponse(response.content, status= response.status_code)
+
+def GekoAPI_exchangeRates(request):
+    response= requests.get( url + "/v3/exchange_rates")
+
+    return HttpResponse(response.content, status= response.status_code)
+#print(request.GET)
+def GekoAPI_search(request):
+    response= requests.get(url + "/v3/search", params=request.GET)
+
+    return HttpResponse(response.content, status= response.status_code)
+
+def GekoAPI_searchTrending(request):
+    response= requests.get(url + "/v3/search/trending")
+
+    return HttpResponse(response.content, status= response.status_code)
+
+def GekoAPI_global(request):
+    response= requests.get(url + "/v3/global")
+
+    return HttpResponse(response.content, status= response.status_code)
+
 
 def GekoAPI_coins_markets(request):
-    response= requests.get(url+'/v3/coins/markets',params=request.GET)
+    response= requests.get(url+'/v3/coins/markets', params=request.GET)
 
-    return HttpResponse(response.content,status= response.status_code)
+    return HttpResponse(response.content, status= response.status_code)
 
 
 
